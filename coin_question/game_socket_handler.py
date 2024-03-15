08b23640cda085c302f36bc1add5027a69dcd1d5
a <a>
2024-03-15 13:44:26 +0000

    making ID unique per connection

--- coin_question/game_socket_handler.py
+++ coin_question/game_socket_handler.py
@@ -1,10 +1,11 @@
 import socketserver
+import uuid
 
 from . import game
 
 
 def generate_game_socket_handler(
-    game_id: str, game_size: int, faulty_coin: int
+    game_size: int, faulty_coin: int
 ) -> type[socketserver.BaseRequestHandler]:
     class GameSocketHandler(socketserver.BaseRequestHandler):
         def handle(self) -> None:
@@ -12,6 +13,7 @@ def generate_game_socket_handler(
             Construct an instance of the game connected to this socket
             and run the game loop
             """
+            game_id = uuid.uuid4().hex  # Unique game ID per connection
             self.game = game.CoinGame(
                 game_id=game_id,
                 sock=self.request,
